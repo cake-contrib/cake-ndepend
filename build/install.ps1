22e0dcb51deb981d79956d328c721bdb2f5e32e6
joaoasrosa <joaoasrosa@gmail.com>
2017-12-17 21:04:03 +0100

    [implement] add the install script

--- build/install.ps1
+++ build/install.ps1
@@ -1,5 +1,10 @@
-nuget install secure-file -ExcludeVersion
-If(Test-Path ENV:secret-key)
+nuget install secure-file -ExcludeVersion;
+If((Test-Path $env:package-key) -and (Test-Path $env:package-pwd))
 {
-    secure-file\tools\secure-file -decrypt buil\secret.ext.enc -secret ENV:secret-key
+    secure-file\tools\secure-file -decrypt lib\package.zip.enc -secret $env:package-key;
+    choco install 7zip.portable -y;
+    7z e lib\package.zip -o"lib\package" -p"$ENV:package-pwd";
+    
+    $packagePath = Resolve-Path .\lib\package;
+    $env:PATH += ";$packagePath";
 }
